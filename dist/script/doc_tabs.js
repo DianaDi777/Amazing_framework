b1ff32176c68c4aff4188b3846d726fd3b12f933
DianaDi777 <tikhonovskova@gmail.com>
2020-05-24 00:04:27 -0700

    4 menu pages_ start

---
+++ dist/script/doc_tabs.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+  $("#docTabs")
+    .find("a")
+    .on("click", function (e) {
+      e.preventDefault();
+
+      //   $("#critterDesciption").load("../data/turtle.html");
+
+      var $desc = $("#docDesciption");
+
+      switch ($(this).attr("href")) {
+        case "00.html":
+          $desc.load("typo.html");
+          break;
+        case "01.html":
+          $desc.load("../data/turtle.html");
+          break;
+
+        case "02.html":
+          $desc.load("../data/shark.html");
+          break;
+        case "03.html":
+          $desc.load("../data/seal.html");
+          break;
+        case "03.html":
+          $desc.load("../data/seal.html");
+          break;
+        case "03.html":
+          $desc.load("../data/seal.html");
+          break;
+        case "06.html":
+          $desc.load("doc/icons.html");
+          break;
+      }
+    });
+}); //end doc ready
