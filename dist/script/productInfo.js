6b161a4a15cbe55815571e6d61638991a1ba9386
DianaDi777 <tikhonovskova@gmail.com>
2020-05-29 16:35:56 -0700

    product card + example linked

--- dist/script/productInfo.js
+++ dist/script/productInfo.js
@@ -23,3 +23,15 @@ function teaCost() {
   num2 = 10;
   document.getElementById("totalPI").innerHTML = num1 * num2;
 }
+//prevent default behavior - no jump when you click on link
+const defaultPI = document.querySelectorAll(".sharePI");
+
+defaultPI[0].addEventListener("click", function (event) {
+  event.preventDefault();
+});
+
+const defaultPIBUY = document.querySelectorAll(".buyPI");
+
+defaultPIBUY[0].addEventListener("click", function (event) {
+  event.preventDefault();
+});
